1197fcdae541cafad3f0b09bc6cf1ce9706a7c4c
Zirkfied <273987506@qq.com>
2016-10-27 15:43:43 +0800

    ZFScan

--- ZFScanCode/ZFScanCode/ZFScan/ZFScanViewController.m
+++ ZFScanCode/ZFScanCode/ZFScan/ZFScanViewController.m
@@ -19,6 +19,8 @@
 @property (nonatomic, strong) NSMutableArray * metadataObjectTypes;
 /** 遮罩层 */
 @property (nonatomic, strong) ZFMaskView * maskView;
+/** 取消按钮 */
+@property (nonatomic, strong) UIButton * cancelButton;
 
 @end
 
@@ -56,6 +58,19 @@
 - (void)addUI{
     self.maskView = [[ZFMaskView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
     [self.view addSubview:self.maskView];
+    
+    //取消按钮
+    CGFloat cancel_width = 100;
+    CGFloat cancel_height = 35;
+    CGFloat cancel_xPos = (CGRectGetWidth(self.maskView.frame) - cancel_width) / 2;
+    CGFloat cancel_yPos = CGRectGetHeight(self.maskView.frame) - cancel_height - 30;
+    
+    self.cancelButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    self.cancelButton.frame = CGRectMake(cancel_xPos, cancel_yPos, cancel_width, cancel_height);
+    [self.cancelButton setTitle:@"取消" forState:UIControlStateNormal];
+    [self.cancelButton setTintColor:YSWhite];
+    [self.cancelButton addTarget:self action:@selector(cancelAction) forControlEvents:UIControlEventTouchUpInside];
+    [self.maskView addSubview:self.cancelButton];
 }
 
 /**
@@ -99,7 +114,25 @@
         [self.session stopRunning];
         AVMetadataMachineReadableCodeObject * metadataObject = metadataObjects.firstObject;
         self.returnScanBarCodeValue(metadataObject.stringValue);
+        
+        if (self.navigationController) {
+            [self.navigationController popViewControllerAnimated:YES];
+        }else{
+            [self dismissViewControllerAnimated:YES completion:nil];
+        }
+    }
+}
+
+#pragma mark - 取消事件
+
+/**
+ * 取消事件
+ */
+- (void)cancelAction{
+    if (self.navigationController) {
         [self.navigationController popViewControllerAnimated:YES];
+    }else{
+        [self dismissViewControllerAnimated:YES completion:nil];
     }
 }
 
